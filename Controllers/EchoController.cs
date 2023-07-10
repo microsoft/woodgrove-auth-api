3d7a3b803d30fdad3b89e2380186bf3581df4bab
Yoel Hor <yoelhor@live.com>
2023-07-11 02:37:59 +0300

    Echo service

--- Controllers/EchoController.cs
+++ Controllers/EchoController.cs
@@ -1,43 +1,43 @@
-// using Microsoft.AspNetCore.Authorization;
-// using Microsoft.AspNetCore.Mvc;
-// using woodgroveapi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using woodgroveapi.Models;
 
-// namespace woodgroveapi.Controllers;
+namespace woodgroveapi.Controllers;
 
 
-// //[Authorize]
-// [ApiController]
-// [Route("[controller]")]
-// public class EchoController : ControllerBase
-// {
-//     private readonly ILogger<EchoController> _logger;
+//[Authorize]
+[ApiController]
+[Route("[controller]")]
+public class EchoController : ControllerBase
+{
+    private readonly ILogger<EchoController> _logger;
 
-//     public EchoController(ILogger<EchoController> logger)
-//     {
-//         _logger = logger;
-//     }
+    public EchoController(ILogger<EchoController> logger)
+    {
+        _logger = logger;
+    }
 
-//     [HttpPost(Name = "Echo")]
-//     public async Task<object> PostAsync()
-//     {
-//         _logger.LogInformation($"#### call to: {this.GetType().Name}");
+    [HttpPost(Name = "Echo")]
+    public async Task<object> PostAsync()
+    {
+        _logger.LogInformation($"#### call to: {this.GetType().Name}");
 
-//         // Validate that REST API received a bearer token in the authorization header.
-//         if (Request.Headers.Authorization.Count == 0)
-//         {
-//             _logger.LogInformation("#### authorization header not found");
-//         }
-//         else
-//         {
-//             _logger.LogInformation($"#### authorization header: {Request.Headers.Authorization[0]}");
-//         }
+        // Validate that REST API received a bearer token in the authorization header.
+        if (Request.Headers.Authorization.Count == 0)
+        {
+            _logger.LogInformation("#### authorization header not found");
+        }
+        else
+        {
+            _logger.LogInformation($"#### authorization header: {Request.Headers.Authorization[0]}");
+        }
 
-//         // Echo the input data
-//         string requestBody = await new StreamReader(this.Request.Body).ReadToEndAsync();
+        // Echo the input data
+        string requestBody = await new StreamReader(this.Request.Body).ReadToEndAsync();
 
-//         _logger.LogInformation($"#### {requestBody}");
+        _logger.LogInformation($"#### {requestBody}");
 
-//         return "Echo";
-//     }
-// }
+        return "Echo";
+    }
+}
 
